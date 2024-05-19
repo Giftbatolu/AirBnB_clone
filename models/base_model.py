881897970f1304255febbab870deaf61e08ef5e3
Giftbatolu <sekinatoyelami@gmail.com>
2024-05-19 11:01:01 -0700

    Airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,25 @@ from datetime import datetime
 
 class BaseModel:
     """ The base class """
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         """ The class constructors that initialise instance """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if kwargs:
+
+            dfmt = "%Y-%m-%dT%H:%M:%S.%f"
+
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], dfmt)
+
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ The print function of the class. """
